570b0b7e58168a8fe4d146ff013129d1e58a8332
sinsister <sinsisteral@gmail.com>
2026-02-04 18:19:56 +0330

    first commit.

---
+++ bavandpour.cpp
@@ -0,0 +1,10 @@
+#including <iostream>
+using namespace std;
+{
+    int a, b, c, d ,e, sum, ave;
+    cout<<"pls enter 5 number :";
+    cin>>a>>b>>x>>d>>e;
+    sum: a+b+c+d+e;
+    cout<<"sum :"<<sum<<endl<<"ave :"<<ave;
+    return 0;
+}
