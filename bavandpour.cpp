a53b87aec3e3a6148c71fab40acf80eb9e62eebe
AryanBavandpour <sinsisteral@gmail.com>
2026-02-04 18:57:22 +0330

    variable change

--- bavandpour.cpp
+++ bavandpour.cpp
@@ -1,10 +1,10 @@
 #including <iostream>
 using namespace std;
 {
-    int a, b, c, d ,e, sum, ave;
+    int a, b, c, d ,f, sum, ave;
     cout<<"pls enter 5 number :";
-    cin>>a>>b>>x>>d>>e;
-    sum: a+b+c+d+e;
+    cin>>a>>b>>x>>d>>f;
+    sum: a+b+c+d+f;
     cout<<"sum :"<<sum<<endl<<"ave :"<<ave;
     return 0;
 }
